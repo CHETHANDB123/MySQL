848b0b26b95c548cd20a3c1a2204500ca0a5e0fa
CHETHAN D B <chethandb.cse@gmail.com>
2026-04-22 11:03:40 +0530

    Parent Key

---
+++ 22-04-26 (3).sql
@@ -0,0 +1,6 @@
+use startersql;
+
+INSERT INTO users(id,name, email, gender, date_of_birth, salary) value
+-- (1,'vvv','vvvv@gmail.com','male','2000-09-09',50000);
+(9,'ww','ww@gmail.com','female','1990-08-07',30000);
+select * from users;
